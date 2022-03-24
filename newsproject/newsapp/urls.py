204999781e7487d69949a0d3815588c58e87836b
Albina Kamalova <alshynggys@mail.ru>
2022-03-24 16:25:41 +0600

    Lecture 3 Code Examples. Changes in news.views.py, news.urls.py. Added templates folder in newsapp application folder.

--- newsproject/newsapp/urls.py
+++ newsproject/newsapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import *
 
+app_name = 'newsapp'
 urlpatterns = [
     path('', index, name='index'),
+    path('articles/article/<int:id>', get_article_by_id, name='get_article_by_id'),
+    path('articles/search/<str:text>', search_article_by_text, name='search_article_by_text'),
+    path('articles/archive/', archive, name='archive'),
 ]
--- newsproject/newsapp/views.py
+++ newsproject/newsapp/views.py
@@ -1,6 +1,45 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+
+    articles = Article.objects.order_by('-article_date')
+
+    return render(request, 'newsapp/index.html', {"latest_articles":articles})
+
+
+def get_article_by_id(request, id):
+    # try:
+    #     print(request, id)
+    #     article = Article.objects.get(pk=id)
+    # except:
+    #     raise Http404(f"No such article with id {id}")
+    # return HttpResponse(f"Article text with article id {id} <br> {article.article_text}")
+
+    article = get_object_or_404(Article, pk=id)
+    return render(request, 'newsapp/article.html', {"article":article})
+
+def search_article_by_text(request, text):
+    articles = list(Article.objects.filter(article_text__contains=text).values_list())
+    print(articles)
+    try:
+        result=""
+        for art in articles:
+            print("Here")
+            result += f"<h3># {articles.index(art)}: {art[1]}</h3><br>"
+        return HttpResponse(f"Search Results: <br> {result}")
+    except:
+         return HttpResponse(f"No such articles with text: {text}")
+
+def archive(request):
+    try:
+        articles = Article.objects.all()
+        result=""
+        for art in articles:
+            result += f"<h3># {art.id}: {art.article_text}</h3><br>"
+        return HttpResponse(f"Search Results: <br> {result}")
+    except:
+        return HttpResponse(f"No such articles has been added yet")
